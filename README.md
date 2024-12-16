601df3a1b8f4ec29fa5965c195d70af057c0c2b0
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2024-12-16 08:45:26 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### challenge done! Sean Eidref Natividad
+
 # URL Shortener Microservice
 
 This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
--- index.js
+++ index.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const short_url_arr=[]
+
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -18,6 +23,32 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', (req, res, next) => {
+  const originalURL = req.body.url;
+  const urlObject = new URL(originalURL);
+  dns.lookup(urlObject.hostname, (err, address, family) => {
+    if(err) {
+      res.json({ error: 'invalid url' })
+      return;
+    } 
+    short_url_arr.push(req.body.url)
+    res.json({original_url:req.body.url,short_url:short_url_arr.length})
+  })
+})
+
+app.get('/api/shorturl/:url',(req,res)=>{
+  if (!isNaN(req.params.url)) {
+    const index =  parseInt(req.params.url)
+    const redirectToUrl = short_url_arr[index-1]
+    if (redirectToUrl) {
+      res.redirect(redirectToUrl); 
+      return;
+    }
+  }
+  res.json({ error: 'invalid url' })
+ 
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
